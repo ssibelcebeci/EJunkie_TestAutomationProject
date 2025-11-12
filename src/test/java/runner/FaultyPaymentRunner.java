944e706583887b52cf0ddef61d96216b11cdc63a
ssibelcebeci <ssibelcebeci@gmail.com>
2025-11-13 00:17:40 +0400

    add faulty payment, and runner

---
+++ src/test/java/runner/FaultyPaymentRunner.java
@@ -0,0 +1,14 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = {
+                "src/test/resources/features/faultyPayment.feature",
+        },
+        glue = {"stepDefinitions","hooks"},
+        plugin = {"pretty","html:target/cucumber-report.html"}
+)
+public class FaultyPaymentRunner extends AbstractTestNGCucumberTests {
+}
